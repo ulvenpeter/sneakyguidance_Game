f2821ceaec2a0ac002a0e7b6fc072abddc241562
SteezyFriis <sprettkalotten@hotmail.com>
2018-03-06 15:25:29 +0100

    fmod stuffs

---
+++ Plugins/FMODStudio/Intermediate/Build/Win64/UE4Editor/Inc/FMODStudio/FMODEventControlSectionTemplate.generated.h
@@ -0,0 +1,27 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef FMODSTUDIO_FMODEventControlSectionTemplate_generated_h
+#error "FMODEventControlSectionTemplate.generated.h already included, missing '#pragma once' in FMODEventControlSectionTemplate.h"
+#endif
+#define FMODSTUDIO_FMODEventControlSectionTemplate_generated_h
+
+#define HostProject_Plugins_FMODStudio_Source_FMODStudio_Private_Sequencer_FMODEventControlSectionTemplate_h_14_GENERATED_BODY \
+	friend FMODSTUDIO_API class UScriptStruct* Z_Construct_UScriptStruct_FFMODEventControlSectionTemplate(); \
+	FMODSTUDIO_API static class UScriptStruct* StaticStruct(); \
+	FORCEINLINE static uint32 __PPO__ControlCurve() { return STRUCT_OFFSET(FFMODEventControlSectionTemplate, ControlCurve); } \
+	typedef FMovieSceneEvalTemplate Super;
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID HostProject_Plugins_FMODStudio_Source_FMODStudio_Private_Sequencer_FMODEventControlSectionTemplate_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
